8390d2be6ac4310acca24f516fc671819d6825ff
RenanCSO <renan.c.s.o@gmail.com>
2021-04-12 21:31:49 -0300

    Initial commit

---
+++ src/test/java/com/renancso/fullstackcourse/FullStackCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renancso.fullstackcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
